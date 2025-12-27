471306fc24648e39de2e3739a6da251bce5bef62
Caleb Morgan <calebmorgan@Calebs-MacBook-Pro.local>
2025-12-27 18:26:27 -0500

    feat: Created new app

---
+++ AdventofCode2025Tests/AdventofCode2025Tests.swift
@@ -0,0 +1,17 @@
+//
+//  AdventofCode2025Tests.swift
+//  AdventofCode2025Tests
+//
+//  Created by Caleb Morgan on 12/27/25.
+//
+
+import Testing
+@testable import AdventofCode2025
+
+struct AdventofCode2025Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
